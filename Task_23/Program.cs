6cfa816b1166449b37f47cb2f4705d7350bbf67f
Dmitriy <potapidze.mos@mail.ru>
2022-10-13 17:00:06 +0300

    Добавил решение задачи №23

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -9,3 +9,18 @@
 // 3 | 27
 // 4 | 64
 // 5 | 125
+
+
+void CubeNumbers(int num)
+{
+    int i = 1;
+    Console.WriteLine($"Таблица кубов чисел от 1 до {num}:");
+    while (i <= num)
+    {
+        Console.WriteLine($"\t {i} | {Math.Pow(i, 3)} ");
+        i++;
+    }
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CubeNumbers(number);
